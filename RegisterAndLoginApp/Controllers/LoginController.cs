bbbf521b78b75fd91e82c90da77b94a7cd736d7d
laxmi <laxmi@DESKTOP-UH9VVUU>
2023-07-24 16:54:07 +0545

    Add project files.

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            
+            if (userModel.Username == "Laxmi Khanal" &&  userModel.Password == "abc")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View(" LoginFailure", userModel);
+            }
+        }
+    }
+}
